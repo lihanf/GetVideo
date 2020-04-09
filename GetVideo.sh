9e3ceb5c3896eb334c0d2f45fa8cdfe5319bfdc5
lihanf <57121758+lihanf@users.noreply.github.com>
2020-04-10 00:50:00 +0800

    Add files via upload

--- GetVideo.sh
+++ GetVideo.sh
@@ -9,18 +9,11 @@ BASEDIR=$(dirname "$0")
 echo $BASEDIR
 echo "Download..."
 sh $BASEDIR/Download.sh
-# osascript -e 'tell app "System Events" to display dialog "'Download\ complete\ for\ ${NAME}.\ Proceed\ to\ Concatenation?'"'
-if [ $? == 0 ]; then
-	echo "Concatenating..."
-	sh $BASEDIR/Concatenation.sh
-	# osascript -e 'tell app "System Events" to display dialog "'Concatenation\ complete\ for\ ${NAME}.\ Proceed\ to\ Deleting?'"'
-	if [ $? == 0 ]; then
-		echo "Deleting..."
-		sh $BASEDIR/Deleting.sh
-		echo $NAME Download completed.
-	fi
-
-fi
+echo "Concatenating..."
+sh $BASEDIR/Concatenation.sh
+echo "Deleting..."
+sh $BASEDIR/Deleting.sh
+echo $NAME Download completed.
 unset NAME
 # unset newplayspeed
 # unset newPTS
